d348e5e8f4f79b10b9078f65130f531460b48a2b
Johan Back <johan.backs@gmail.com>
2020-02-18 11:59:40 -0300

    chore: add axios

---
+++ Modulo 4/main.js
@@ -0,0 +1,34 @@
+/* var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://api.github.com/users/johanbacks");
+xhr.send(null);
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 ){
+    console.log(JSON.parse(xhr.responseText))
+  }
+} */
+var minhaPromise = function() {
+  return new Promise(function(resolve , reject){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET" , "https://api.github.com/users/johanbacks");
+    xhr.send(null);
+
+   xhr.onreadystatechange = function() {
+     if(xhr.readyState === 4) {
+       if (xhr.status === 200) {
+         resolve(JSON.parse(xhr.responseText));
+       } else {
+         reject("Erro na reuquisição")
+       }
+     }
+   }
+  });
+}
+axios.get("https://api.github.com/users/johanbacks")
+  .then(function(response){
+    console.log(response);
+  })
+  .catch(function(error){
+    console.warn(error)
+  });
